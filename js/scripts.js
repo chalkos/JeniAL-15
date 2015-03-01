225a8abb62a22c837352c47800491cd36a2c8466
Bruno Ferreira <chalkos@chalkos.net>
2015-03-01 14:56:35 +0000

    alterado countdown

--- js/scripts.js
+++ js/scripts.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
 $(document).ready(function() {
 	"use strict";
 	$("#countdown").countdown({
-		date: "10 march 2015 12:00:00", /** Enter new date here **/
+		date: "10 April 2015 12:00:00", /** Enter new date here **/
 		format: "on"
 	},
 	function() {
