e5ca77f9a2255ab4ddb92c12ca8309dd9b21c47c
Bruno Ferreira <chalkos@chalkos.net>
2015-04-03 23:36:38 +0100

    comprimir css e js

--- config.rb
+++ config.rb
@@ -81,10 +81,10 @@ set :slim, {
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :asset_hash
@@ -94,8 +94,6 @@ configure :build do
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
-
-  coiso = 'yes!!'
 
   ignore "/index.original.html"
 end
